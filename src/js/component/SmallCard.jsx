46fd8276f87f10456bbcf35394568c5ac7ba7c49
joselyngm11 <joselyngm.47@gmail.com>
2022-09-09 21:32:47 +0000

    SmallCard component REACT

--- src/js/component/SmallCard.jsx
+++ src/js/component/SmallCard.jsx
@@ -4,16 +4,16 @@ import imagen from "../../img/img.png"
 const SmallCard = ()=>{
     return(
         <div className="card">
-                    <img className="card-img-top" src= {imagen} ></img>
-                    <div className="card-body">
-                        <h4 className="project-name mt-4">Card Title</h4>
-                        <p className="porfolio-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam viverra euismod odio, gravida 
-                            pellentesque urna varius vitae.
-                        </p>
-                    </div>
-                    <div className="card-footer">
-                    <a href="#" className="boton btn btn-primary">Find Out More!</a>
-                    </div>
+            <img className="card-img-top" src= {imagen} ></img>
+            <div className="card-body">
+                <h4 className="project-name mt-4">Card Title</h4>
+                <p className="porfolio-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam viverra euismod odio, gravida 
+                    pellentesque urna varius vitae.
+                </p>
+            </div>
+            <div className="card-footer">
+                <a href="#" className="boton btn btn-primary">Find Out More!</a>
+            </div>
         </div>
     )
 }
